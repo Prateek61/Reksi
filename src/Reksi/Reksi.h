10996b6f7aeaf6332df1768fc20c3f617b4b78ba
Prateek61 <prateekpoudel61@gmail.com>
2024-11-13 11:49:05 +0545

    format:

--- src/Reksi/Reksi.h
+++ src/Reksi/Reksi.h
@@ -29,210 +29,218 @@
 
 namespace Reksi
 {
-    // Memory stuff
-    template<typename T>
-    using Scope = std::unique_ptr<T>;
-    template<typename T, typename ... Args>
-    constexpr Scope<T> CreateScope(Args&& ... args)
-    {
-        return std::make_unique<T>(std::forward<Args>(args)...);
-    }
+	// Memory stuff
+	template <typename T>
+	using Scope = std::unique_ptr<T>;
 
-    template<typename T>
-    using Ref = std::shared_ptr<T>;
-    template<typename T, typename ... Args>
-    constexpr Ref<T> CreateRef(Args&& ... args)
-    {
-        return std::make_shared<T>(std::forward<Args>(args)...);
-    }
+	template <typename T, typename... Args>
+	constexpr Scope<T> CreateScope(Args&&... args)
+	{
+		return std::make_unique<T>(std::forward<Args>(args)...);
+	}
 
-    template <typename T1, typename T2>
-    constexpr Ref<T1> StaticRefCast(const Ref<T2>& ref)
-    {
+	template <typename T>
+	using Ref = std::shared_ptr<T>;
+
+	template <typename T, typename... Args>
+	constexpr Ref<T> CreateRef(Args&&... args)
+	{
+		return std::make_shared<T>(std::forward<Args>(args)...);
+	}
+
+	template <typename T1, typename T2>
+	constexpr Ref<T1> StaticRefCast(const Ref<T2>& ref)
+	{
 		return std::static_pointer_cast<T1>(ref);
-    }
+	}
 
-    class ResourceManager;
-    typedef uint32_t ResourceIDType;
+	class ResourceManager;
+	using ResourceIDType = uint32_t;
 
-    enum class ResourceStatus
-    {
-        NotLoaded = 0,
-        Loading,
-        Loaded
-    };
+	enum class ResourceStatus
+	{
+		NotLoaded = 0,
+		Loading,
+		Loaded
+	};
 
-    struct ResourceData
-    {
-        using LoadFunc = std::function<Ref<void>(const std::filesystem::path&)>;
+	struct ResourceData
+	{
+		using LoadFunc = std::function<Ref<void>(const std::filesystem::path&)>;
 
-        const std::filesystem::path Path;
-        Ref<void> Data;
-        ResourceStatus Status;
-        bool Reloading = false;
-        const LoadFunc Loader;
+		const std::filesystem::path Path;
+		Ref<void> Data;
+		ResourceStatus Status;
+		bool Reloading = false;
+		const LoadFunc Loader;
 
-        RK_AUTO_MUTEX
+		RK_AUTO_MUTEX
 
-        bool Reload();
-        bool Load();
-        bool Unload();
-        ResourceStatus GetStatus();
-        bool IsLoaded();
-        bool IsReloading();
-        template <typename T>
-        Ref<T> GetData();
-    };
+		bool Reload();
+		bool Load();
+		bool Unload();
+		ResourceStatus GetStatus();
+		bool IsLoaded();
+		bool IsReloading();
+		template <typename T>
+		Ref<T> GetData();
+	};
 
-    // Resource Class
-    template <typename T>
-    class Resource
-    {
-    public:
-        Ref<T> GetRef();
+	// Resource Class
+	template <typename T>
+	class Resource
+	{
+	public:
+		Ref<T> GetRef();
 		const Ref<T> GetRef() const;
 
-        T& operator*();
+		T& operator*();
 		const T& operator*() const;
 
-        ResourceStatus GetStatus();
-        bool IsLoaded();
+		ResourceStatus GetStatus();
+		bool IsLoaded();
 		bool IsReloading();
 
-        bool Reload();
-        bool Load();
-        bool Unload();
+		bool Reload();
+		bool Load();
+		bool Unload();
 
 		std::filesystem::path GetPath() const;
 		ResourceData::LoadFunc GetLoader() const;
 
-    private:
-        Resource(ResourceIDType id, Ref<ResourceData> data);
+	private:
+		Resource(ResourceIDType id, Ref<ResourceData> data);
 
-        ResourceIDType m_ID;
-        Ref<ResourceData> m_Data;
+		ResourceIDType m_ID;
+		Ref<ResourceData> m_Data;
 
-        friend class ResourceManager;
-    };
+		friend class ResourceManager;
+	};
 
-    // ResourceManager Class
-    class ResourceManager
-    {
-    public:
-        ResourceManager() = default;
-        ~ResourceManager() = default;
+	// ResourceManager Class
+	class ResourceManager
+	{
+	public:
+		ResourceManager() = default;
+		~ResourceManager() = default;
 
-        template <typename T>
-        Resource<T> GetResource(const std::filesystem::path& path, ResourceData::LoadFunc loader, bool immediateLoad = false);
-        template <typename T>
-        Resource<T> GetResource(const std::filesystem::path& path, bool immediateLoad = false);
-        template <typename T>
-        Resource<T> GetResource(ResourceIDType id);
+		template <typename T>
+		Resource<T> GetResource(const std::filesystem::path& path, ResourceData::LoadFunc loader,
+		                        bool immediateLoad = false);
+		template <typename T>
+		Resource<T> GetResource(const std::filesystem::path& path, bool immediateLoad = false);
+		template <typename T>
+		Resource<T> GetResource(ResourceIDType id);
 
-    private:
-        RK_AUTO_MUTEX
+	private:
+		RK_AUTO_MUTEX
 
-    	ResourceIDType m_CurrID = 1;
+		ResourceIDType m_CurrID = 1;
 		std::unordered_map<ResourceIDType, Ref<ResourceData>> m_Resources;
-        std::unordered_map<std::filesystem::path, uint32_t> m_ResourcePaths;
-    };
+		std::unordered_map<std::filesystem::path, uint32_t> m_ResourcePaths;
+	};
 }
 
 #pragma region definition
 namespace Reksi
 {
-    template<typename T>
-    Resource<T> ResourceManager::GetResource(const std::filesystem::path& path, ResourceData::LoadFunc loader, bool immediateLoad)
-    {
-        ResourceIDType id = 0;
+	template <typename T>
+	Resource<T> ResourceManager::GetResource(const std::filesystem::path& path, ResourceData::LoadFunc loader,
+	                                         bool immediateLoad)
+	{
+		ResourceIDType id = 0;
 
-        {
-	        RK_AUTO_LOCK_SCOPE
+		{
+			RK_AUTO_LOCK_SCOPE
 
-        	auto it = m_ResourcePaths.find(path);
-			if (it != m_ResourcePaths.end())
+			auto it = m_ResourcePaths.find(path);
+			if ( it != m_ResourcePaths.end() )
 			{
 				id = it->second;
 			}
-        }
+		}
 
-		if (id) return GetResource<T>(id);
+		if ( id ) return GetResource<T>(id);
 
-        // Create new resource
-		Ref<ResourceData> data = CreateRef<ResourceData>(ResourceData{ path, nullptr, ResourceStatus::NotLoaded, false, loader });
+		// Create new resource
+		Ref<ResourceData> data = CreateRef<ResourceData>(ResourceData{
+			path, nullptr, ResourceStatus::NotLoaded, false, loader
+		});
 
-        {
-            RK_AUTO_LOCK_SCOPE
+		{
+			RK_AUTO_LOCK_SCOPE
 
-        	id = m_CurrID++;
+			id = m_CurrID++;
 			// Add to resource paths
-            m_ResourcePaths[path] = id;
+			m_ResourcePaths[path] = id;
 			// Add to resources
-            m_Resources[id] = data;
-        }
+			m_Resources[id] = data;
+		}
 
-        if (immediateLoad)
-        {
-            data->Load();
-        }
+		if ( immediateLoad )
+		{
+			data->Load();
+		}
 
 		return Resource<T>{id, data};
-    }
+	}
 
-    template<typename T>
-    Resource<T> ResourceManager::GetResource(const std::filesystem::path& path, bool immediateLoad)
-    {
-        ResourceIDType id = 0;
+	template <typename T>
+	Resource<T> ResourceManager::GetResource(const std::filesystem::path& path, bool immediateLoad)
+	{
+		ResourceIDType id = 0;
 
-        {
-	        RK_AUTO_LOCK_SCOPE
+		{
+			RK_AUTO_LOCK_SCOPE
 
-        	auto it = m_ResourcePaths.find(path);
-			if (it != m_ResourcePaths.end())
+			auto it = m_ResourcePaths.find(path);
+			if ( it != m_ResourcePaths.end() )
 			{
 				id = it->second;
 			}
-        }
+		}
 
-		if (id) return GetResource<T>(id);
+		if ( id ) return GetResource<T>(id);
 
 		// Use the constructor the T type to create a loader
 		ResourceData::LoadFunc loader = [](const std::filesystem::path& path) -> Ref<void>
-			{
-                return CreateRef<T>(path);
-			};
+		{
+			return CreateRef<T>(path);
+		};
 		// Create new resource
-		Ref<ResourceData> data = CreateRef<ResourceData>(ResourceData{ path, nullptr, ResourceStatus::NotLoaded, false, loader });
+		Ref<ResourceData> data = CreateRef<ResourceData>(ResourceData{
+			path, nullptr, ResourceStatus::NotLoaded, false, loader
+		});
 
-        {
+		{
 			RK_AUTO_LOCK_SCOPE
 
-        	id = m_CurrID++;
+			id = m_CurrID++;
 			// Add to resource paths
-            m_ResourcePaths[path] = id;
+			m_ResourcePaths[path] = id;
 			// Add to resources
 			m_Resources[id] = data;
-        }
+		}
 
 		return Resource<T>{id, data};
-    }
+	}
 
-    template<typename T>
-    Resource<T> ResourceManager::GetResource(ResourceIDType id)
-    {
-        Ref<ResourceData> data;
+	template <typename T>
+	Resource<T> ResourceManager::GetResource(ResourceIDType id)
+	{
+		Ref<ResourceData> data;
 
-        {
-	        RK_AUTO_LOCK_SCOPE
-        	auto it = m_Resources.find(id);
-			if (it == m_Resources.end())
+		{
+			RK_AUTO_LOCK_SCOPE
+			auto it = m_Resources.find(id);
+			if ( it == m_Resources.end() )
 			{
 				return Resource<T>(0, nullptr);
 			}
-			data = it->second;
-        }
+		    data = it->second;
+		}
 
-        return Resource<T>{id, data};
-    }
+		return Resource<T>{id, data};
+	}
 }
 #pragma endregion
--- src/main.cpp
+++ src/main.cpp
@@ -3,39 +3,7 @@
 #define LOG(x) std::cout << #x << ": " << (x) << "\n"
 #define LOGI(x) std::cout << #x << "\n"
 
-// Forward declare templated class C1
-template <typename T>
-class C1;
-
-class C2
-{
-public:
-	C2(int a = 0) : a(a) {}
-
-private:
-	template <typename T>
-	friend class C1<T>;
-
-	int a;
-};
-
-template <typename T>
-class C1
-{
-public:
-	C1(T a) : a(a) {}
-
-	void PrintC2(C2 c2)
-	{
-		LOG(c2.a);
-	}
-private:
-    T a;
-};
-
 int main()
 {
-	C1<int> c1(5);
-	C2 c2(10);
-	c1.PrintC2(c2);
+	LOGI(Hello);
 }
